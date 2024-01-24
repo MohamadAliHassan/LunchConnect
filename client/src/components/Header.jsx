3f601695a87419ac248a8ed60babe74eefe26a11
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2024-01-24 14:58:32 +0100

    worked  hamburger menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <h3>
-        Lunch<span>Connect</span>
-      </h3>
+      <div className="hamburger-menu">
+        <div
+          className={`menu-icon ${isOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {isOpen && (
+          <div className="menu-items">
+            <a href="/">Home</a>
+            <a href="/about">Achievements</a>
+            <a href="/contact">Profile</a>
+            <a href="/contact">Logout</a>
+          </div>
+        )}
+      </div>
     </>
   );
 };
