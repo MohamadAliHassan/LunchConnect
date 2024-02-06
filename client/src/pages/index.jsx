2d9f58665857e188a605ee29f098377a24248a23
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2024-02-06 14:07:51 +0100

    worked on event page

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -3,5 +3,6 @@ export { AchievementsPage } from "./AchievementsPage/AchievementsPage";
 export { LandingPage } from "./LandingPage/LandingPage";
 export { LoginPage } from "./LoginPage/LoginPage";
 export { RegisterPage } from "./RegisterPage/RegisterPage";
-import { HomePage } from "./HomePage/HomePage";
-import { ChatPage } from "./ChatPage/ChatPage";
+export { HomePage } from "./HomePage/HomePage";
+export { ChatPage } from "./ChatPage/ChatPage";
+export { EventsPage } from "./EventsPage/EventsPage";
