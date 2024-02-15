0b4d0a3bd556fd16c0017452a62ba5beb3b740eb
Lucas Magnusson <lucasmyeet@gmail.com>
2024-02-15 20:26:45 +0100

    quick fix for achievement model

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import { RegisterUser } from "../controllers/RegisterUser.js";
 import { fetchAllAchievements } from "../controllers/fetchAchievements.js";
+import insertAchievements from "../controllers/achievementSeeder.js"
 
 const router = express.Router();
 
