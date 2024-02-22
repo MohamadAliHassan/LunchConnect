ee6ba711889e60020eac01f84b72c0f4c006fd1d
Lucas Magnusson <lucasmyeet@gmail.com>
2024-02-22 13:13:13 +0100

    sessionservice

---
+++ client/src/utils/sessionService.js
@@ -0,0 +1,14 @@
+function getToken() {
+    return sessionStorage.getItem("token");
+  }
+  
+  function setToken(token) {
+    sessionStorage.setItem("token", token);
+  }
+  
+  function clearSession() {
+    sessionStorage.clear();
+  }
+  
+  export default { getToken, setToken, clearSession };
+  
