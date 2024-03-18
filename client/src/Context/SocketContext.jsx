1d53193547b3cc496d35e3268d6b2f7ad7b435c6
Lucas Magnusson <lucasmyeet@gmail.com>
2024-03-18 10:20:17 +0100

    changed some socket stuff

--- client/src/Context/SocketContext.jsx
+++ client/src/Context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:3000", {
+      const socket = io("ws://socket", {
         query: {
           userId: authUser._id,
         },
