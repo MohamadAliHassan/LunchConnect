51d8e21d2191a834a37553cbada609e809b78857
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2024-03-18 11:14:17 +0100

    bug fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,24 @@
-import { reactEasierViteConfig as revc_ } from 'react-easier/vite-config'
+import { reactEasierViteConfig as revc_ } from "react-easier/vite-config";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(revc_({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:3000/",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(
+  revc_({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://127.0.0.1:3000/",
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  build: {
-    outDir: "../backend/dist",
-  },
-  ssr: {
-    noExternal: ["react", "react-dom"],
-  },
-}));
+    build: {
+      outDir: "../client/dist",
+    },
+    ssr: {
+      noExternal: ["react", "react-dom"],
+    },
+  })
+);
