f0a29c4022f98ce8a35fc2e5483726d6cfcb3e3b
Ironboy <ironboy@users.noreply.github.com>
2024-03-20 13:29:07 +0100

    Changed to just / as socket path in backend

--- backend/utils/socket.js
+++ backend/utils/socket.js
@@ -6,7 +6,7 @@ const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-	path: "/socket"
+	path: "/"
 });
 
 export const getReceiverSocketId = (receiverId) => {
