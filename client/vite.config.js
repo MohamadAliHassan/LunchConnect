6c063359a7dacedf908d887eb5d93d6e8b38baaa
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2024-03-22 15:01:32 +0100

    changed ssh to env

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,26 +2,28 @@ import { reactEasierViteConfig as revc_ } from "react-easier/vite-config";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(revc_({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://lunch-connect-v7s2.vercel.app/",
-        changeOrigin: true,
-        secure: 'auto',
+export default defineConfig(
+  revc_({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+          secure: "auto",
+        },
+        "/socket.io": {
+          target: "ws://localhost:3000",
+          ws: true,
+          changeOrigin: true,
+        },
       },
-      '/socket.io': {
-        target: 'ws://lunch-connect-v7s2.vercel.app/',
-        ws: true,
-        changeOrigin: true,
+      build: {
+        outDir: "../client/dist",
+      },
+      ssr: {
+        noExternal: ["react", "react-dom"],
       },
     },
-    build: {
-      outDir: "../client/dist",
-    },
-    ssr: {
-      noExternal: ["react", "react-dom"],
-    },
-  }})
+  })
 );
--- utils/jwtUtil.js
+++ utils/jwtUtil.js
@@ -1,32 +1,35 @@
 import jwt from "jsonwebtoken";
 import fs from "fs";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+// import path, { dirname } from "path";
+// import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const keysDir = path.join(__dirname, "../config/keys/");
+// const keysDir = path.join(__dirname, "../etc/secrets/");
 
-const privateKey = fs.readFileSync(keysDir + "private.pem");
-const publicKey = fs.readFileSync(keysDir + "public.pem");
+// const privateKey = fs.readFileSync(keysDir + "private.pem");
+// const publicKey = fs.readFileSync(keysDir + "public.pem");
+
+const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, "\n");
+const publicKey = process.env.PUBLIC_KEY.replace(/\\n/g, "\n");
 
 export const createToken = (userid, res) => {
-  console.log("signing token")
-	const token = jwt.sign({ userid }, privateKey, {
+  console.log("signing token");
+  const token = jwt.sign({ userid }, privateKey, {
     algorithm: "RS256",
-		expiresIn: "15d",
-	});
-  console.log("token signed")
+    expiresIn: "15d",
+  });
+  console.log("token signed");
 
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
-  })
+  });
 
   return token;
-}
+};
 
 export const validateToken = (token) => {
   return jwt.verify(token, publicKey);
-}
+};
